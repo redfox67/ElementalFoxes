84fa23468b238b3a1f89fc7b42b970b16f2c3027
Garyn McGuire <garynwmcguire@gmail.com>
2023-07-19 21:54:07 -0400

    updated and fixed Display Case tile entity.
    *note*
    DO NOT SHIFT CLICK INTO OR OUT OF DISPLAY CASE

--- ElementalFoxes/src/main/java/com/Harlefox/elementalfoxes/common/block/DisplayCaseBlock.java
+++ ElementalFoxes/src/main/java/com/Harlefox/elementalfoxes/common/block/DisplayCaseBlock.java
@@ -1,5 +1,8 @@
 package com.Harlefox.elementalfoxes.common.block;
 
+import com.Harlefox.elementalfoxes.common.te.DisplayCaseTileEntity;
+import com.Harlefox.elementalfoxes.core.init.TileEntityTypesInit;
+
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
@@ -7,7 +10,7 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.inventory.container.INamedContainerProvider;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
@@ -15,38 +18,35 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.network.NetworkHooks;
 
-public class DisplayCaseBlock extends Block{
-
+public class DisplayCaseBlock extends Block {
 	public DisplayCaseBlock() {
-		super(AbstractBlock.Properties.of(Material.METAL, MaterialColor.COLOR_GRAY).strength(15f)
+		super(AbstractBlock.Properties.of(Material.METAL, MaterialColor.COLOR_GRAY)
 				.sound(SoundType.METAL));
 	}
-
+	
 	@Override
 	public boolean hasTileEntity(BlockState state) {
 		return true;
 	}
-
+	
 	@Override
 	public TileEntity createTileEntity(BlockState state, IBlockReader world) {
-		return com.Harlefox.elementalfoxes.core.init.TileEntityTypesInit.DISPLAY_CASE_ENTITY_TYPE.get().create();
+		return TileEntityTypesInit.DISPLAY_CASE_ENTITY_TYPE.get().create();
 	}
-
+	
 	@SuppressWarnings("deprecation")
 	@Override
-	public ActionResultType use(BlockState p_225533_1_, World p_225533_2_, BlockPos p_225533_3_,
-			PlayerEntity p_225533_4_, Hand p_225533_5_, BlockRayTraceResult p_225533_6_) {
-		if (p_225533_2_.isClientSide) {
-			return ActionResultType.SUCCESS;
-		} else {
-			INamedContainerProvider inamedcontainerprovider = this.getMenuProvider(p_225533_1_, p_225533_2_,
-					p_225533_3_);
-			if (inamedcontainerprovider != null) {
-				p_225533_4_.openMenu(inamedcontainerprovider);
+	public ActionResultType use(BlockState state, World worldIn, BlockPos pos, PlayerEntity player,
+			Hand handIn, BlockRayTraceResult hit) {
+		if (!worldIn.isClientSide()) {
+			TileEntity te = worldIn.getBlockEntity(pos);
+			if (te instanceof DisplayCaseTileEntity) {
+				NetworkHooks.openGui((ServerPlayerEntity) player, (DisplayCaseTileEntity) te, pos);
+				return ActionResultType.SUCCESS; 
 			}
-
-			return ActionResultType.CONSUME;
 		}
+		return super.use(state, worldIn, pos, player, handIn, hit);
 	}
 }
